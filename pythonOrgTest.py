2e45540a9cf5cec7f1cd4a15702ea3ac76d42be9
BannAnn <tennant66@rambler.ru>
2022-04-16 12:02:37 +0300

    new branch banna added

--- pythonOrgTest.py
+++ pythonOrgTest.py
@@ -1,14 +1,14 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-#
-# driver = webdriver.Firefox()
-# driver.get("http://www.python.org")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
 
 import unittest
 import time
@@ -20,6 +20,7 @@ from tkcalendar import Calendar, DateEntry
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 
 class PythonOrgSearch(unittest.TestCase):
 
@@ -39,6 +40,7 @@ class PythonOrgSearch(unittest.TestCase):
 
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == "__main__":
     unittest.main()

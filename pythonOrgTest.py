68a60b5d49430a7f0a54185c0a8f6e84d87802b7
Natalia <natali_lari@ymail.com>
2022-04-20 23:18:04 +0200

    cleanup

--- pythonOrgTest.py
+++ pythonOrgTest.py
@@ -1,21 +1,5 @@
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-
-#driver = webdriver.Firefox()
-#driver.get("http://www.python.org")
-#assert "Python" in driver.title
-#elem = driver.find_element_by_name("q")
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.RETURN)
-#assert "No results found." not in driver.page_source
-#driver.close()
-
-#import tkinter as tk
-#from tkinter import ttk
-#from tkcalendar import Calendar, DateEntry
-
 import unittest
-import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys

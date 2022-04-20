f6284dbfa9b8ccb7187e5ea9650d3278b47f0e35
Natalia <natali_lari@ymail.com>
2022-04-20 23:14:08 +0200

    first test

--- pythonOrgTest.py
+++ pythonOrgTest.py
@@ -1,30 +1,30 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-#
-# driver = webdriver.Firefox()
-# driver.get("http://www.python.org")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+
+#driver = webdriver.Firefox()
+#driver.get("http://www.python.org")
+#assert "Python" in driver.title
+#elem = driver.find_element_by_name("q")
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
+#driver.close()
+
+#import tkinter as tk
+#from tkinter import ttk
+#from tkcalendar import Calendar, DateEntry
 
 import unittest
 import time
-
-import tkinter as tk
-from tkinter import ttk
-from tkcalendar import Calendar, DateEntry
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path='/Users/a1/Desktop/PythonOrgTest/chromedriver')
 
     def test_search_in_python_org(self):
         driver = self.driver
@@ -39,6 +39,7 @@ class PythonOrgSearch(unittest.TestCase):
 
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == "__main__":
     unittest.main()
